b8d28031d8eab70191db4e7705b2753087b094d1
martinlopez1234 <57772762+martinlopez1234@users.noreply.github.com>
2022-09-27 00:31:31 -0300

    update

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -3,11 +3,12 @@ import React from "react";
 //create your first component
 const Navbar = () => {
 	return (
-<div className="navbar-colaborativo">
-    <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-        <a className="navbar-brand " href="#">Start Bootstrap</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse justify-content-end " id="navbarNav">
         <ul className="navbar-nav ">
@@ -25,8 +26,10 @@ const Navbar = () => {
             </li>
         </ul>
         </div>
-    </nav>
 </div>
+</nav>
+       
+  
     
    
 	);
