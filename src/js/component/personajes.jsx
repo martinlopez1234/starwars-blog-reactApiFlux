c431fc7b937cbeab65f54678190607072b9d34f6
martinlopez1234 <57772762+martinlopez1234@users.noreply.github.com>
2022-10-11 13:25:23 -0300

    update

--- src/js/component/personajes.jsx
+++ src/js/component/personajes.jsx
@@ -27,10 +27,32 @@ useEffect(()=>{
 	return (
 
 <>
-<p>personajes</p>
+<nav class="navbar navbar-expand-lg navbar-dark bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img width="100px" src="https://logos-world.net/wp-content/uploads/2020/11/Star-Wars-Logo.png"/></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="btn-group">
+  <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+    Favorites
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <li><hr class="dropdown-divider"/></li>
+    <li><a class="dropdown-item" href="#">Separated link</a></li>
+  </ul>
+</div>
+</div>
+</nav>
+<div className="container">
+<h2 className="text-danger mt-3">Personajes</h2>
+</div>
 {informacion && informacion?.results.map(({name})=>{
   {console.log("name",name)}
-return <div className="row">
+return <div className="container"><div className="row mt-4">
 <div class="col-lg-3 mb-4 text-center ">
 <div class="card" >
 <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpdvATSPHH4Tt6TJyc4JFtJy0zMwSOj57xpRvOZfLGfuErJXWlIZWLjxbXItN4s1Drt-w&usqp=CAU" class="card-img-top" alt="..."/>
@@ -43,6 +65,7 @@ return <div className="row">
 </div>
 
 
+</div>
 </div>
 })
 
