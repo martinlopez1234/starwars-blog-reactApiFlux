0c761bf31c9cc94d5341b691daf1cc858299a35d
martinlopez1234 <57772762+martinlopez1234@users.noreply.github.com>
2022-10-13 21:39:41 -0300

    cambios

--- src/js/index.js
+++ src/js/index.js
@@ -10,15 +10,15 @@ import "../styles/index.css";
 import People from "./component/personajes.jsx";
 import Planetas from "./component/Planetas.jsx";
 
+import Nabvar from "./component/Nabvar.jsx";
 //render your react application
 const Pagina = () => {
     //here you have to return expected html using the properties being passed to the component
     return (
         <>
+        <Nabvar/>
         <People/>
         <Planetas/>
-        
-       
         </>
         
     );
