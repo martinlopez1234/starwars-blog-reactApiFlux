bc281401ff3d65d38610d14e23296881bc27cfbb
martinlopez1234 <57772762+martinlopez1234@users.noreply.github.com>
2022-10-15 11:57:41 -0300

    cambios

--- src/js/page/PersonajesD.jsx
+++ src/js/page/PersonajesD.jsx
@@ -7,11 +7,30 @@ const About = () => {
 	return (
 
 <>
+<nav class="navbar navbar-expand-lg navbar-dark bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="/Home"><img width="100px" src="https://logos-world.net/wp-content/uploads/2020/11/Star-Wars-Logo.png"/></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="btn-group">
+  <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+    Favorites
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <li><hr class="dropdown-divider"/></li>
+    <li><a class="dropdown-item" href="#">Separated link</a></li>
+  </ul>
+</div>
+</div>
+</nav>
         
         <h2>welcome to mami about</h2>
         <p>estas en about</p>
     
-        <Link to="/Home">regresar a home</Link>
         
     
     </>
