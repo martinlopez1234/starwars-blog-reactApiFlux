43bad39b285a7e699078eefc65305fa4befe0df7
martinlopez1234 <57772762+martinlopez1234@users.noreply.github.com>
2022-10-26 19:06:36 -0300

    cambios

--- src/js/page/Home.jsx
+++ src/js/page/Home.jsx
@@ -9,6 +9,9 @@ const Home = () => {
 const [personajes,setPersonajes] = useState();
 const [planetas,setPlanetas] = useState();
 const [vehiculos,setVehiculos] = useState();
+const [planetas2,setPlanetas2] = useState();
+const [personajes2,setPersonajes2] = useState();
+const [vehiculos2,setVehiculos2] = useState();
 
 // get Planetas
 useEffect(()=>{
@@ -25,8 +28,18 @@ useEffect(()=>{
     })
     .catch(error => console.log('error', error));
     
-
+   
+    
+    fetch("https://swapi.dev/api/planets/", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result)
+        setPlanetas2(result)
+      })
+      .catch(error => console.log('error', error));
 },[])
+
+
 
 // get Personajes
 useEffect(()=>{
@@ -43,6 +56,15 @@ useEffect(()=>{
     })
     .catch(error => console.log('error', error));
     
+   
+    
+    fetch("https://swapi.dev/api/people/", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result)
+        setPersonajes2(result)
+      })
+      .catch(error => console.log('error', error));
 
 },[])
 // get vehiculos
@@ -59,6 +81,14 @@ useEffect(()=>{
       setVehiculos(result)
     })
     .catch(error => console.log('error', error));
+
+    fetch("https://swapi.dev/api/vehicles/", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result)
+      setVehiculos2(result)
+    })
+    .catch(error => console.log('error', error));
     
 
 },[])
@@ -73,15 +103,24 @@ useEffect(()=>{
 <h2 className="text-danger mt-3">Personajes</h2>
 </div>
 <div className="container mt-4">
-<div class=" row row-cols-2 row-cols-md-5 g-2">
+<div className=" row row-cols-2 row-cols-md-5 g-2">
 
 {personajes && personajes.results.map((item, index)=>{
-  {console.log("name",name)}
-return <div class="col">
-  <div class="card" key={index} >
-  <img  src={`https://starwars-visualguide.com/assets/img/characters/${item.uid}.jpg`} class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title">{item.name}</h5>
+  const a =personajes2.results.filter((elem)=>{
+    return elem.name == item.name;
+    
+    }
+    
+    )
+  
+return <div className="col">
+  <div className="card" key={index} >
+  <img  src={`https://starwars-visualguide.com/assets/img/characters/${item.uid}.jpg`} className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">{item.name}</h5>
+      <p>Gender: {a[0].gender}</p>
+      <p>Hair Color: {a[0].hair_color}</p>
+      <p>Eye-Color: {a[0].eye_color}</p>
      
       <button className="btn btn-outline-primary"><Link to={`PersonajesD/${item.uid}`}>ir about</Link></button>
       <button className="far fa-heart ml-3"></button>
@@ -100,17 +139,24 @@ return <div class="col">
 <h2 className="text-danger mt-3">Planetas</h2>
 </div>
 <div className="container mt-4">
-<div class=" row row-cols-2 row-cols-md-5 g-2">
+<div className=" row row-cols-2 row-cols-md-5 g-2">
 
 {planetas && planetas?.results.map((item,index)=>{
-  {console.log("name",name)}
-return <div class="col" key={index}>
-  <div class="card">
-  <img  src={`https://starwars-visualguide.com/assets/img/planets/${item.uid}.jpg`} class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title">{item.name}</h5>
-      <p class="card-text">Population:{item.population}</p>
-      <p class="card-text">Terrain:{item.terrain}</p>
+const a =planetas2.results.filter((elem)=>{
+return elem.name == item.name;
+
+}
+
+)
+
+return <div className="col" key={index}>
+  <div className="card">
+  <img  src={`https://starwars-visualguide.com/assets/img/planets/${item.uid}.jpg`} className="card-img-top" alt="..."/>
+    <div className="card-body">
+  
+      <h5 className="card-title">{item.name}</h5>
+      <p className="card-text">Population: {a[0].population}</p>
+      <p className="card-text">Terrain: {a[0].terrain}</p>
       <button className="btn btn-outline-primary"><Link to={`PlanetasD/${item.uid}`}>ir about</Link></button>
       <button className="far fa-heart ml-3"></button>
     </div>
@@ -128,15 +174,23 @@ return <div class="col" key={index}>
 <h2 className="text-danger mt-3">Vehiculos</h2>
 </div>
 <div className="container mt-4">
-<div class=" row row-cols-2 row-cols-md-5 g-2">
+<div className=" row row-cols-2 row-cols-md-5 g-2">
 
 {vehiculos && vehiculos.results.map((item, index)=>{
-  {console.log("name",name)}
-return <div class="col">
-  <div class="card" key={index} >
-  <img  src={`https://starwars-visualguide.com/assets/img/vehicles/${item.uid}.jpg`} class="card-img-top" alt="..."/>
-    <div class="card-body">
-      <h5 class="card-title">{item.name}</h5>
+  const a =vehiculos2.results.filter((elem)=>{
+    return elem.name == item.name;
+    
+    }
+    
+    )
+  
+return <div className="col">
+  <div className="card" key={index} >
+  <img  src={`https://starwars-visualguide.com/assets/img/vehicles/${item.uid}.jpg`} className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">{item.name}</h5>
+      <p>cargo capacity: {a[0].cargo_capacity}</p>
+     
      
       <button className="btn btn-outline-primary"><Link to={`VehiculosD/${item.uid}`}>ir about</Link></button>
       <button className="far fa-heart ml-3"></button>
@@ -152,7 +206,7 @@ return <div class="col">
 </div>
 
  
-<footer class="mt-7 p-4 text-center bg-dark text-white" >
+<footer className="mt-7 p-4 text-center bg-dark text-white" >
     Derechos Reservados© Martin Lopez
         
         </footer>
