4369dc1ec1b4833b28055fdd9342ca4fff38b289
martinlopez1234 <57772762+martinlopez1234@users.noreply.github.com>
2022-10-26 19:27:19 -0300

    cambios

--- src/js/component/Nabvar.jsx
+++ src/js/component/Nabvar.jsx
@@ -9,22 +9,22 @@ const Nabvar = () => {
 
 <>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-light">
-      <div class="container">
-        <a class="navbar-brand" href="#"><img width="100px" src="https://logos-world.net/wp-content/uploads/2020/11/Star-Wars-Logo.png"/></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-dark bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="#"><img width="100px" src="https://logos-world.net/wp-content/uploads/2020/11/Star-Wars-Logo.png"/></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="btn-group">
-  <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        <div className="btn-group">
+  <button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
     Favorites
   </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-    <li><hr class="dropdown-divider"/></li>
-    <li><a class="dropdown-item" href="#">Separated link</a></li>
+  <ul className="dropdown-menu">
+    <li><a className="dropdown-item" href="#">Action</a></li>
+    <li><a className="dropdown-item" href="#">Another action</a></li>
+    <li><a className="dropdown-item" href="#">Something else here</a></li>
+    <li><hr className="dropdown-divider"/></li>
+    <li><a className="dropdown-item" href="#">Separated link</a></li>
   </ul>
 </div>
 </div>
@@ -37,3 +37,4 @@ const Nabvar = () => {
 };
 
 export default Nabvar;
+
