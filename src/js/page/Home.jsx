7beddfcfdd800c116af863e723b6a3498533bffa
martinlopez1234 <57772762+martinlopez1234@users.noreply.github.com>
2022-11-02 13:20:40 -0300

    ca

--- src/js/page/Home.jsx
+++ src/js/page/Home.jsx
@@ -98,7 +98,7 @@ return <div className="col" key={index}>
      
      
       <button className="btn btn-outline-primary"><Link to={`PersonajesD/${item.uid}`}>ir about</Link></button>
-      <button className="far fa-heart ml-3"></button>
+      <button onClick={() => alert(item.name)} className="btn btn-outline-warning float-end far fa-heart ml-3"></button>
     </div>
   </div>
 </div>
@@ -129,7 +129,7 @@ return <div className="col" key={index}>
     
     
       <button className="btn btn-outline-primary"><Link to={`PlanetasD/${item.uid}`}>ir about</Link></button>
-      <button className="far fa-heart ml-3"></button>
+      <button className="btn btn-outline-warning float-end far fa-heart ml-3"></button>
     </div>
   </div>
 </div>
@@ -163,7 +163,7 @@ return <div className="col" key={index}>
      
      
       <button className="btn btn-outline-primary"><Link to={`VehiculosD/${item.uid}`}>ir about</Link></button>
-      <button className="far fa-heart ml-3"></button>
+      <button className="btn btn-outline-warning float-end far fa-heart ml-3"></button>
     </div>
   </div>
 </div>
