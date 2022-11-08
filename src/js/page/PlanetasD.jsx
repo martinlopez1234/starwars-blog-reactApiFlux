dc630da29b2369fd83e63d21c23a6f1db9471ce6
martinlopez1234 <57772762+martinlopez1234@users.noreply.github.com>
2022-11-08 18:15:08 -0300

    cambios

--- src/js/page/PlanetasD.jsx
+++ src/js/page/PlanetasD.jsx
@@ -1,6 +1,7 @@
 import React,{useEffect,useState} from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import Nabvar from "../component/Nabvar.jsx";
 //create your first component
 
 const PlanetasD = () => {
@@ -21,26 +22,7 @@ const PlanetasD = () => {
 	return (
 /* Se agrega Nabvar y contenido de la pagina */
 <>
-<nav className="navbar navbar-expand-lg navbar-dark bg-light">
-      <div className="container">
-        <a className="navbar-brand" href="/"><img width="100px" src="https://logos-world.net/wp-content/uploads/2020/11/Star-Wars-Logo.png"/></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="btn-group">
-  <button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-    Favorites 0
-  </button>
-  <ul className="dropdown-menu">
-    <li><a className="dropdown-item" href="#">Action</a></li>
-    <li><a className="dropdown-item" href="#">Another action</a></li>
-    <li><a className="dropdown-item" href="#">Something else here</a></li>
-    <li><hr className="dropdown-divider"/></li>
-    <li><a className="dropdown-item" href="#">Separated link</a></li>
-  </ul>
-</div>
-</div>
-</nav>
+<Nabvar/>
 <center><div className="container m-4 d-flex justify-content-center">
 <div className="row my-3">
           <div className="col-md-4">
